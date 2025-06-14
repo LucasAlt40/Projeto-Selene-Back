9145ce6c7c4aa48312ac6afef9ad78f68533c3e4
LucasAlt40 <lucas.alt40@gmail.com>
2025-06-13 21:23:19 -0300

    first commit

---
+++ src/main/java/br/selene/projectseleneback/ProjectSeleneBackApplication.java
@@ -0,0 +1,13 @@
+package br.selene.projectseleneback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSeleneBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSeleneBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/selene/projectseleneback/ProjectSeleneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.selene.projectseleneback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSeleneBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
