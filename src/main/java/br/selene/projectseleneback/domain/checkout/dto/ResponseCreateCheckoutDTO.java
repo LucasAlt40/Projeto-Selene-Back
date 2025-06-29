22cf43ea0c7d312ef1b44d34489b6d9f3121a6c0
Eduardo Pires <epc4129@gmail.com>
2025-06-29 18:12:12 -0300

    feat: criando entidade checkout

---
+++ src/main/java/br/selene/projectseleneback/domain/checkout/dto/ResponseCreateCheckoutDTO.java
@@ -0,0 +1,13 @@
+package br.selene.projectseleneback.domain.checkout.dto;
+
+public class ResponseCreateCheckoutDTO {
+    private String linkCheckout;
+
+    public String getLinkCheckout() {
+        return linkCheckout;
+    }
+
+    public void setLinkCheckout(String linkCheckout) {
+        this.linkCheckout = linkCheckout;
+    }
+}
--- src/main/java/br/selene/projectseleneback/domain/checkout/service/ICheckoutService.java
+++ src/main/java/br/selene/projectseleneback/domain/checkout/service/ICheckoutService.java
@@ -1,5 +1,8 @@
 package br.selene.projectseleneback.domain.checkout.service;
 
+import br.selene.projectseleneback.domain.checkout.dto.ResponseCreateCheckoutDTO;
+
 public interface ICheckoutService {
 
+    ResponseCreateCheckoutDTO createCheckout (int OrderId);
 }
