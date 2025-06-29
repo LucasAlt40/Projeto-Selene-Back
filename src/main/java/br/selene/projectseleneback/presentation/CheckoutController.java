e542e16eb12b538956e64dac1fe13a03c083938b
LucasAlt40 <lucas.alt40@gmail.com>
2025-06-29 14:56:44 -0300

    fix: remoção de código do lombok

--- src/main/java/br/selene/projectseleneback/presentation/CheckoutController.java
+++ src/main/java/br/selene/projectseleneback/presentation/CheckoutController.java
@@ -7,15 +7,11 @@ import br.selene.projectseleneback.model.dto.ResponseGatewayDTO;
 import br.selene.projectseleneback.model.mappers.CheckoutMapper;
 import br.selene.projectseleneback.service.CheckoutService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.Disposable;
 
 @RestController
 @RequestMapping("/checkout")
 @CrossOrigin(origins = "*")
-@Slf4j
 public class CheckoutController {
 
     private final CheckoutService checkoutService;
@@ -39,15 +35,11 @@ public class CheckoutController {
     }
 
     @PostMapping("/payment_notification_urls")
-    public Void newLog(@RequestBody Object object){
-        log.debug("payment_notification_urls\n",object.toString());
-        return null;
+    public void newLog(@RequestBody Object object){
     }
     @PostMapping("/notification_urls")
-    public Void newLog2(@RequestBody Object object){
+    public void newLog2(@RequestBody Object object){
 
-        log.debug("notification_urls\n",object.toString());
-        return null;
     }
 
 
