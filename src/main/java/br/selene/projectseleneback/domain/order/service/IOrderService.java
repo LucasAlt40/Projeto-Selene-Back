7c6ab043680047bf7bbc89533282b5aed122430b
LucasAlt40 <lucas.alt40@gmail.com>
2025-06-29 16:50:06 -0300

    feat: criando dtos para notification controller

--- src/main/java/br/selene/projectseleneback/domain/order/service/IOrderService.java
+++ src/main/java/br/selene/projectseleneback/domain/order/service/IOrderService.java
@@ -1,6 +1,6 @@
 package br.selene.projectseleneback.domain.order.service;
 
-import br.selene.projectseleneback.presentation.dto.RequestCheckoutNotificationDTO;
+import br.selene.projectseleneback.presentation.dto.notification.RequestCheckoutNotificationDTO;
 
 public interface IOrderService {
     void updateOrderStatus(RequestCheckoutNotificationDTO request);
--- src/main/java/br/selene/projectseleneback/presentation/dto/RequestCheckoutNotificationDTO.java
+++
@@ -1,7 +0,0 @@
-package br.selene.projectseleneback.presentation.dto;
-
-public record RequestCheckoutNotificationDTO(
-        int reference_id,
-        String status
-) {
-}
