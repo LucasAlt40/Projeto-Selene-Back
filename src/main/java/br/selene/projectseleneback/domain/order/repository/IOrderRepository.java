9b868841f48a134d5a69dad48be1df810fc7207d
Eduardo Pires <epc4129@gmail.com>
2025-06-30 21:07:28 -0300

    feat: iniciando implementacao OrderRepository

--- src/main/java/br/selene/projectseleneback/domain/order/repository/IOrderRepository.java
+++ src/main/java/br/selene/projectseleneback/domain/order/repository/IOrderRepository.java
@@ -1,10 +1,14 @@
 package br.selene.projectseleneback.domain.order.repository;
 
 import br.selene.projectseleneback.domain.order.Order;
+import br.selene.projectseleneback.domain.order.TicketOrder;
+
+import java.util.List;
 
 public interface IOrderRepository {
 
-    Order findById(int id);
+    Order findById(int orderId);
     // TODO salvar ordem header e items e atualizar quantidade de tickets disponivel no avaliable
     Boolean save(Order order);
+    List<TicketOrder> findItemsByOrderId(int orderId);
 }
