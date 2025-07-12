7c5e902e78cb819d720613d0cc67312c58a3fdc3
LucasAlt40 <lucas.alt40@gmail.com>
2025-07-12 20:24:59 -0300

    refactor: removendo public

--- src/main/java/br/selene/projectseleneback/domain/ticketCategory/service/ITicketCategoryService.java
+++ src/main/java/br/selene/projectseleneback/domain/ticketCategory/service/ITicketCategoryService.java
@@ -9,13 +9,13 @@ import br.selene.projectseleneback.domain.ticketCategory.dto.UpdateTicketCategor
 import br.selene.projectseleneback.infra.exception.TicketOperationException;
 
 public interface ITicketCategoryService {
-	public Page<TicketCategoryDTO> findAll(SearchTicketCategoryDTO searchTicketCategoryDTO);
+	Page<TicketCategoryDTO> findAll(SearchTicketCategoryDTO searchTicketCategoryDTO);
 
-	public TicketCategoryDTO create(CreateTicketCategoryDTO createTicketCategoryDTO);
+	TicketCategoryDTO create(CreateTicketCategoryDTO createTicketCategoryDTO);
 
-	public TicketCategoryDTO update(Long TicketCategoryId, UpdateTicketCategoryDTO updateTicketCategoryDTO);
+	TicketCategoryDTO update(Long TicketCategoryId, UpdateTicketCategoryDTO updateTicketCategoryDTO);
 
-	public void reserveTicket(Long ticketCategoryId, int quantity) throws TicketOperationException;
+	void reserveTicket(Long ticketCategoryId, int quantity) throws TicketOperationException;
 
-	public void releaseTicket(Long ticketCategoryId, int quantity) throws TicketOperationException;
+	void releaseTicket(Long ticketCategoryId, int quantity) throws TicketOperationException;
 }
