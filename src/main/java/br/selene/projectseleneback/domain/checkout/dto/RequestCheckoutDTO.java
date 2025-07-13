36b2ed57a913de6c0c8ce0fd84bbb9eb95a527fb
Eduardo Pires Carvalho <epc44129@gmail.com>
2025-07-13 15:22:40 -0300

    feat: altera campos criacao checkout

--- src/main/java/br/selene/projectseleneback/domain/checkout/dto/RequestCheckoutDTO.java
+++ src/main/java/br/selene/projectseleneback/domain/checkout/dto/RequestCheckoutDTO.java
@@ -4,9 +4,12 @@ package br.selene.projectseleneback.domain.checkout.dto;
 import org.springframework.http.client.reactive.ClientHttpRequest;
 import org.springframework.web.reactive.function.BodyInserter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record RequestCheckoutDTO(
+        String reference_id,
+        String expiration_date,
         CustomerCheckoutDTO  customer,
         List<ItemsCheckoutDTO> items,
         List<PaymentMethodCheckoutDTO> payment_methods,
